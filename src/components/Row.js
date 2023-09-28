e4c4a9539c32274d936b4d2a28c15b6b822b2b7a
Br-yk <bryk.defi@gmail.com>
2023-09-28 20:19:12 +0100

    title and overview addition

--- src/components/Row.js
+++ src/components/Row.js
@@ -15,22 +15,30 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n-1) + '...' : string;
+}
+
   return (
     <div className='row'>
       <h2>{title}</h2>
       <div className='poster_row'>
         {movies.map((movie) => (
           ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
-            <img
+            <div>
+              <img
               className={`poster ${isLargeRow && 'large_poster'}`}
               src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
               key={movie.id}
               alt={movie.title}
             />
+            <p class='row_title'>{movie?.title || movie?.name || movie?.original_name}</p>
+            <p class='row_description'>{truncate(movie?.overview, 80)}</p>
+            </div>  
           )
-        ))}
-        <p>{movies?.title}</p>
+        ))}  
       </div>
+      
     </div>
   );
 }
